e61c8532b93879bc0d7a04d6614ce5bfc71f4eb5
profWilliamArte <arteagawilliam13@hotmail.com>
2024-02-07 10:30:19 -0400

    2er commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://profWilliamArte.github.io',
-	base: 'astro01',
+	base: '/astro01',
 });
